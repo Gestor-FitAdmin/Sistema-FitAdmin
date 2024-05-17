7d8a5e45b5061f4005401a541e80093416aadd45
Leo-Dev <leonardocaimmi1@gmail.com>
2024-05-17 19:30:20 -0300

    Modelacion y prueba de Rutina

--- FitAdmin/src/main/java/org/example/Main.java
+++ FitAdmin/src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import org.example.Enum.EObjetivo;
+import org.example.Modelo.Rutina;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
