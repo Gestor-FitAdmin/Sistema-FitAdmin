de6b26fd46b9d9c0f0b0720ab3075dd08e5d41a7
Fabrizio Golisciano <fabri.vogol@gmail.com>
2024-05-17 21:52:19 -0300

    Clase Actividad actualizada

--- FitAdmin/src/main/java/org/example/Modelo/Actividad.java
+++ FitAdmin/src/main/java/org/example/Modelo/Actividad.java
@@ -1,4 +1,42 @@
 package org.example.Modelo;
 
 public abstract class Actividad {
+    //atributos
+
+    //si hay tiempo agregar atributo precio y sus funcionalidades al igual que Dates
+    /*
+    private double precio;
+    private Calendar fechaInicio;
+    private Calendar fechaVencimiento;
+    private String modalidadDePago;
+    */
+    private int cupos;
+
+
+    //constructores
+
+    public Actividad()
+    {
+        cupos=0;
+    }
+
+    public Actividad(int cupos) {
+        this.cupos = cupos;
+    }
+
+    //get y set
+
+    public int getCupos() {
+        return cupos;
+    }
+
+    public void setCupos(int cupos) {
+        this.cupos = cupos;
+    }
+
+    //metodos
+
+    public abstract String obtenerActividad();
+    //public abstract double calcularPrecioDeUnaClase();
+    //public abstract double calcularPrecioCuota();
 }
