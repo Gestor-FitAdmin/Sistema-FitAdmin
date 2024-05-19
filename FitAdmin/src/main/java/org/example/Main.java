7faacceef3567d32cab053e1e685fa0d6c3064dc
Fabrizio Golisciano <fabri.vogol@gmail.com>
2024-05-19 01:38:03 -0300

    Update Main.java

--- FitAdmin/src/main/java/org/example/Main.java
+++ FitAdmin/src/main/java/org/example/Main.java
@@ -7,8 +7,8 @@ import org.example.Modelo.Rutina;
 public class Main {
     public static void main(String[] args)
     {
-        Rutina rutina = new Rutina(EObjetivo.PERDER_PESO,3);
+        //Rutina rutina = new Rutina(EObjetivo.PERDER_PESO,3);
        //rutina.generarRutinaDelDia("C:\\Users\\Gc\\Desktop\\programacion\\tpFinal3erCuatri\\FitAdmin\\src\\main\\resources\\ejercicios.json",EObjetivo.PERDER_PESO,20);
-        System.out.println(rutina);
+        //System.out.println(rutina);
     }
 }
