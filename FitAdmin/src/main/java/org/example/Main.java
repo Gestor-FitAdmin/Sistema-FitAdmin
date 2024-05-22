db386dead3a2dea9e7d0a7afff7680f62de15b8e
Fabrizio Golisciano <fabri.vogol@gmail.com>
2024-05-21 21:20:57 -0300

    main pruebas

--- FitAdmin/src/main/java/org/example/Main.java
+++ FitAdmin/src/main/java/org/example/Main.java
@@ -12,25 +12,24 @@ import java.io.IOException;
 
 
 public class Main {
-    public static void main(String[] args){
-        Gimnasio gimnasio= new Gimnasio("fitAdmin","Villa urquiza","gonza","velez2do");
+    public static void main(String[] args) {
+        Gimnasio gimnasio = new Gimnasio("fitAdmin", "Villa urquiza", "gonza", "velez2do");
 
-        Cliente cliente = new Cliente("Leo","Caimmi","46012540","masculino",75.5,182.5,"09/07/2004",1,"facundoprocelli@gmail.com",true);
+        Cliente cliente = new Cliente("Leo", "Caimmi", "46012540", "masculino", 75.5, 182.5, "09/07/2004", 1, "facundoprocelli@gmail.com", true);
         Rutina rutina = new Rutina(EObjetivo.GANAR_MUSCULO);
 
-        try {
-            DropBoxAPI dropBoxAPI= new DropBoxAPI();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        /*QrAPI qrAPI= new QrAPI();
+        DropBoxAPI dropBoxAPI;
+
+        dropBoxAPI = new DropBoxAPI();
+
+        QrAPI qrAPI = new QrAPI();
+
 
         gimnasio.crearPDFParaQR(cliente);
 
-        dropBoxAPI.subirPDF("pdfDatosCliente/QRaGenerar.pdf"); // subirlo a dropbox
-        String url= dropBoxAPI.obtenerURL("QRaGenerar"); //recordar subir solo el nombre del archivo que esta en dropbox
-        qrAPI.generarQr(url);*/
-
+        //dropBoxAPI.subirPDF("pdfDatosCliente/QRaGenerar.pdf"); // subirlo a dropbox
+        String url = dropBoxAPI.obtenerURL("cv"); //recordar subir solo el nombre del archivo que esta en dropbox
+        qrAPI.generarQr(url);
 
 
     }
