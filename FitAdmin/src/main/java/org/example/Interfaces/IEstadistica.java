ef4fbc9991d8d9f4c4ea9a39237be00f0c65db5a
Fabrizio Golisciano <fabri.vogol@gmail.com>
2024-05-29 20:34:20 -0300

    Implementacion estadisticas ventana estadisticas

--- FitAdmin/src/main/java/org/example/Interfaces/IEstadistica.java
+++ FitAdmin/src/main/java/org/example/Interfaces/IEstadistica.java
@@ -1,11 +1,13 @@
 package org.example.Interfaces;
 
+import org.example.Enum.ESexo;
+
 public interface IEstadistica
 {
    int  contarTotalClientes();
    int contarClientesActivos();
    int contarClientesInactivos();
-   int contarClientesXGenero(String genero);
+   int contarClientesXGenero(ESexo sexoBuscado);
    int contarClientesXActividad(String actividad);
    double recaudacionTotal();
 }
