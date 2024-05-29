ba045ff2e06e62c8147cc18145649d82c5423e01
Fabrizio Golisciano <fabri.vogol@gmail.com>
2024-05-29 17:01:26 -0300

    comentario eliminado ya que ya lo hicimos

--- FitAdmin/src/main/java/org/example/Interfaces/IMetodosCrud.java
+++ FitAdmin/src/main/java/org/example/Interfaces/IMetodosCrud.java
@@ -2,9 +2,6 @@ package org.example.Interfaces;
 
 public interface IMetodosCrud<E>
 {
-
-    //cambiar nombre de interface a metodos CRUD o similar
-
     boolean agregar(E e);
     boolean archivar(E e);
     E buscar(Integer i);
